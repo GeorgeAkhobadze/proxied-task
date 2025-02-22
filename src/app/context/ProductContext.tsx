39ce8d390901e31c414281695a8ae678b0a99129
Giorgi Akhobadze <89395175+GeorgeAkhobadze@users.noreply.github.com>
2025-02-22 15:18:24 +0400

    feat: setup React Context to store products (#4)

---
+++ src/app/context/ProductContext.tsx
@@ -0,0 +1,29 @@
+"use client";
+import {createContext, useContext, useState} from "react";
+import {Product} from "@/app/graphql/types";
+
+interface ProductContextType {
+    products: Product[];
+    setProducts: (products: Product[]) => void;
+}
+
+const defaultContextValue: ProductContextType = {
+    products: [],
+    setProducts: () => {},
+};
+
+const ProductContext = createContext<ProductContextType>(defaultContextValue);
+
+export function ProductProvider({ children }: { children: React.ReactNode }) {
+    const [products, setProducts] = useState<Product[]>([]);
+
+    return (
+        <ProductContext.Provider value={{ products, setProducts }}>
+            {children}
+        </ProductContext.Provider>
+    );
+}
+
+export function useProducts(): ProductContextType {
+    return useContext(ProductContext);
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import {ApolloWrapper} from "@/app/lib/ApolloClient";
+import {ProductProvider} from "@/app/context/ProductContext";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,7 +30,9 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
           <ApolloWrapper>
+              <ProductProvider>
               {children}
+              </ProductProvider>
           </ApolloWrapper>
       </body>
     </html>
