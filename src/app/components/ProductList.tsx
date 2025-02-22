aa4c8dbdc7c544c2f1c0d436f8d100b1d2cf799d
Giorgi Akhobadze <axobadzegiorgi5@gmail.com>
2025-02-22 15:16:39 +0400

    feat: setup React Context to store products

--- src/app/components/ProductList.tsx
+++ src/app/components/ProductList.tsx
@@ -1,25 +1,33 @@
 "use client";
 
-import React from "react";
+import React, {useEffect} from "react";
 import { useQuery } from "@apollo/client";
 import { GET_PRODUCTS } from "@/app/graphql/queries";
 import { Product } from "@/app/graphql/types";
+import {useProducts} from "@/app/context/ProductContext";
 
 const ProductList: React.FC = () => {
     const token = typeof window !== "undefined" ? localStorage.getItem("token") : null;
+    const { setProducts, products } = useProducts();
 
     const { data, loading, error } = useQuery<{ getProducts: { products: Product[] } }>(GET_PRODUCTS, {
         skip: !token,
     });
 
+    useEffect(() => {
+        if (data?.getProducts?.products.length) {
+            setProducts(data.getProducts.products);
+        }
+    }, [data, setProducts]);
+
     if (!token) return <p>Checking authentication...</p>;
     if (loading) return <p>Loading products...</p>;
     if (error) return <p>Error loading products: {error.message}</p>;
-    if (!data?.getProducts?.products.length) return <p>No products available</p>;
+    if (products.length <= 0) return <p>No products available</p>;
 
     return (
         <ul>
-            {data.getProducts.products.map((product, index) => (
+            {products.map((product, index) => (
                 <li key={index}>
                     {product.title} - ${product.cost}
                 </li>
--- src/app/graphql/types.ts
+++ src/app/graphql/types.ts
@@ -1,4 +1,7 @@
 export type Product = {
     title: string;
     cost: number;
+    isArchived: boolean;
+    availableQuantity: number;
+    _id: number;
 };
