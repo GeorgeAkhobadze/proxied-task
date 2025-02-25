371c0a9fff4192cac03cd50282abc37b6186306a
Giorgi Akhobadze <89395175+GeorgeAkhobadze@users.noreply.github.com>
2025-02-25 15:35:18 +0400

    feat: Checks for product stock and in cart for product card (#13)

--- src/components/Product/ProductCard.tsx
+++ src/components/Product/ProductCard.tsx
@@ -11,10 +11,13 @@ interface ProductProps {
 }
 
 const ProductCard = ({ product }: ProductProps) => {
-  const { addToCart } = useCart();
+  const { addToCart, cart } = useCart();
   const schema = useCartAddItemSchema();
   const [addItem, { loading: addItemLoading }] = useMutation(ADD_ITEM_MUTATION);
   const { showToast } = useToast();
+  const isInCart = cart.items.some((item) => item.product._id === product._id);
+  const isOutOfStock = product.availableQuantity === 0;
+
   const handleAddItem = async () => {
     const input = {
       productId: product._id,
@@ -42,19 +45,31 @@ const ProductCard = ({ product }: ProductProps) => {
   };
 
   return (
-    <li className="border cursor-pointer border-gray-700 rounded-lg p-4 shadow-md bg-gray-800 transition-transform transform hover:scale-105 hover:bg-gray-700">
+    <li
+      className={`border cursor-pointer border-gray-700 rounded-lg p-4 shadow-md bg-gray-800 transition-transform transform ${
+        isOutOfStock
+          ? 'opacity-50 cursor-not-allowed'
+          : 'hover:scale-105 hover:bg-gray-700'
+      }`}
+    >
       <h2 className="text-lg font-semibold text-gray-200">{product.title}</h2>
       <p className="text-gray-400">
         Price: <span className="text-green-400 font-bold">${product.cost}</span>
       </p>
       <p className="text-gray-500">Stock: {product.availableQuantity}</p>
-      <button
-        onClick={() => handleAddItem()}
-        className="mt-4 px-4 py-2 bg-blue-500 text-white rounded"
-        disabled={addItemLoading}
-      >
-        {addItemLoading ? 'Adding...' : 'Add Item'}
-      </button>
+      {isInCart ? (
+        <p className="mt-6 text-gray-400">In Cart</p>
+      ) : isOutOfStock ? (
+        <p className="mt-6 text-red-500 font-bold">Out of Stock</p> // Show "Out of Stock" message
+      ) : (
+        <button
+          onClick={() => handleAddItem()}
+          className="mt-4 px-4 py-2 bg-blue-500 text-white rounded"
+          disabled={addItemLoading}
+        >
+          {addItemLoading ? 'Adding...' : 'Add Item'}
+        </button>
+      )}
     </li>
   );
 };
