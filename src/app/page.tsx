0dcafc1e5ddd10129e9f94f02690a6aaa3e5aaf1
Giorgi Akhobadze <axobadzegiorgi5@gmail.com>
2025-02-27 04:22:11 +0400

    fix: localStorage not defined

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,18 @@
 'use client';
 
 import { useMutation } from '@apollo/client';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { REGISTER_MUTATION } from '@/graphql/mutations';
 import ProductList from '@/components/ProductList';
 
 export default function Home() {
-  const token = localStorage.getItem('token');
+  const [hydrated, setHydrated] = useState(false);
   const [register, { loading: registerLoading, error: registerError }] =
     useMutation(REGISTER_MUTATION);
 
   useEffect(() => {
+    setHydrated(true);
+    const token = localStorage.getItem('token');
     if (!token) {
       register()
         .then((res) => {
@@ -20,6 +22,8 @@ export default function Home() {
         .catch(console.error);
     }
   }, [register]);
+
+  if (!hydrated) return null; // Avoid mismatches during SSR
 
   if (registerLoading) return <p>Registering Visitor...</p>;
   if (registerError) return <p>Error: {registerError.message}</p>;
